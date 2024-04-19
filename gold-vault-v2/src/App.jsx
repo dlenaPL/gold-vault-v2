a223076d7a0e981375f725a54a6dea530f9e1ee8
dlenaPL <dlenarciak01@gmail.com>
2024-04-19 22:36:35 +0200

    Rework of putting props to item card and product page component that it can be passed as an array of objects through the App component and generated accordingly
     Changes to be committed:
    	modified:   gold-vault-v2/src/App.jsx
    	modified:   gold-vault-v2/src/products/ProductCardMini.jsx
    	modified:   gold-vault-v2/src/products/ProductCardMiniPage.jsx

--- gold-vault-v2/src/App.jsx
+++ gold-vault-v2/src/App.jsx
@@ -6,6 +6,82 @@ import ProductCardMiniPage from './products/ProductCardMiniPage'
 import Footer from './footer/Footer'
 import PromoCalculator from './extras/PromoCalculator'
 
+import gb1oz from'./assets/gold-bars/valcambi_1oz_gold_bar.png'
+import gb1g from'./assets/gold-bars/valcambi_1g_gold_bar.png'
+import gb5g from'./assets/gold-bars/valcambi_5g_gold_bar.png'
+import gb10g from'./assets/gold-bars/valcambi_10g_gold_bar.png'
+
+import kangaroo from './assets/gold-coins/1-oz-australian-kangaroo.jpg'
+import philh from './assets/gold-coins/austrian-philharmonicer-2024-1oz-gold-obverse-2.png'
+import brit from './assets/gold-coins/britannia-1-oz-gold-coin-reverse-2024.png'
+import krug from './assets/gold-coins/krugerrand-gold-coin-1-oz-reverse.png'
+
+
+
+const bars = [
+  {
+    id: 1,
+    title: "Sztabka Valcambi",
+    imgSrc: gb1g,
+    weight: "1g",
+    price: "380,00 PLN"
+  },
+  {
+    id: 2,
+    title: "Sztabka Valcambi",
+    imgSrc: gb5g,
+    weight: "5g",
+    price: "1660,00 PLN"
+  },
+  {
+    id: 3,
+    title: "Sztabka Valcambi",
+    imgSrc: gb10g,
+    weight: "10g",
+    price: "3276,00 PLN"
+  },
+  {
+    id: 4,
+    title: "Sztabka Valcambi",
+    imgSrc: gb1oz,
+    weight: "1oz",
+    price: "9917,00 PLN"
+  },
+];
+
+
+const coins = [
+  {
+    id: 101,
+    title: "Australijski Kangur",
+    imgSrc: kangaroo,
+    weight: "1oz",
+    price: "9917,00 PLN"
+  },
+  {
+    id: 102,
+    title: "Złoty Filharmonik",
+    imgSrc: philh,
+    weight: "1oz",
+    price: "9917,00 PLN"
+  },
+  {
+    id: 103,
+    title: "Złota Britannia",
+    imgSrc: brit,
+    weight: "1oz",
+    price: "9917,00 PLN"
+  },
+  {
+    id: 104,
+    title: "Złoty Krugerrand",
+    imgSrc: krug,
+    weight: "1oz",
+    price: "9917,00 PLN"
+  }
+];
+
+
 
 function App() {
 
@@ -13,9 +89,9 @@ function App() {
     <div className='app-container'>
       <Navbar/>
       <Slide/>
-      <ProductCardMiniPage/>
+      <ProductCardMiniPage header="Polecane produkty" products={bars}/>
       <PromoCalculator/>
-      <ProductCardMiniPage/>
+      <ProductCardMiniPage header="Polecane dla Ciebie" products={coins}/>
       <Footer/>
     </div>
    
--- gold-vault-v2/src/products/ProductCardMiniPage.jsx
+++ gold-vault-v2/src/products/ProductCardMiniPage.jsx
@@ -3,72 +3,31 @@ import ProductCardMini from './ProductCardMini'
 import './ProductCardMiniPage.css'
 
 
-import gb1g from'../assets/gold-bars/valcambi_1g_gold_bar.png'
-import gb5g from'../assets/gold-bars/valcambi_5g_gold_bar.png'
-import gb10g from'../assets/gold-bars/valcambi_10g_gold_bar.png'
-import gb1oz from'../assets/gold-bars/valcambi_1oz_gold_bar.png'
+function ProductCardMiniPage({header, products}) {
 
-import kangaroo from '../assets/gold-coins/1-oz-australian-kangaroo.jpg'
-import philh from '../assets/gold-coins/austrian-philharmonicer-2024-1oz-gold-obverse-2.png'
-import brit from '../assets/gold-coins/britannia-1-oz-gold-coin-reverse-2024.png'
-import krug from '../assets/gold-coins/krugerrand-gold-coin-1-oz-reverse.png'
-
-
-const itemsForProdPage = [
-  {
-    id: 1,
-    title: "Sztabka Valcambi",
-    imgSrc: gb1g,
-    weight: "1g",
-    price: "380,00 PLN"
-  },
-  {
-    id: 2,
-    title: "Sztabka Valcambi",
-    imgSrc: gb5g,
-    weight: "5g",
-    price: "1660,00 PLN"
-  },
-  {
-    id: 3,
-    title: "Sztabka Valcambi",
-    imgSrc: gb10g,
-    weight: "10g",
-    price: "3276,00 PLN"
-  },
-  {
-    id: 4,
-    title: "Sztabka Valcambi",
-    imgSrc: gb1oz,
-    weight: "1oz",
-    price: "9917,00 PLN"
-  },
-]
-
-
-
-function ProductCardMiniPage() {
-
-
-  const dataArray = itemsForProdPage.map(bar => 
-  <ProductCardMini 
-    key={bar.id}
-    title={bar.title} 
-    imgSrc={bar.imgSrc}
-    weight={bar.weight}  
-    price={bar.price}
-  />)
-
+  let dataArray = []; 
+  if(products){
+    dataArray = products.map(product => 
+      <ProductCardMini 
+        key={product.id}
+        title={product.title} 
+        imgSrc={product.imgSrc}
+        weight={product.weight}  
+        price={product.price}
+      />)
+  }
+  
+  // {products ? console.log("test items: " + 'object: %O', products ) : null}
 
   return (
+
     <div className='mini-cards-container'>
       <div className='product-promo-text'>
-          <h4>Polecane produkty</h4>
+        <h4>{header}</h4>
       </div>
       <div className='mini-cards-content'>
         {dataArray}
       </div>
-        
     </div>
     
   )
